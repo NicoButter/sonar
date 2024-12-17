fd8b202d0d41758f03987ebabb3590954d1c287d
Nicolás Butterfield <nicobutter@gmail.com>
2024-12-17 20:08:13 -0300

    correciones en los templates

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.landing_page, name='landing_page'),  # Página de aterrizaje
-    # path('login/', views.login_view, name='login'),
-    # path('logout/', views.logout_view, name='logout'),
+    path('login/', views.login_view, name='login'),       # Iniciar sesión
+    path('logout/', views.logout_view, name='logout'),     # Cerrar sesión
     # otras rutas de tu aplicación accounts
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
 
 def landing_page(request):
     return render(request, 'landing_page.html')
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('landing_page')
+        else:
+            # Manejar error de autenticación
+            return render(request, 'login.html', {'error': 'Nombre de usuario o contraseña incorrectos.'})
+    return render(request, 'login.html')
+
+def logout_view(request):
+    logout(request)
+    return redirect('landing_page')
