6cc6d0083e4f977f474e1c7a19c8706b3f1db657
Nicolás Butterfield <nicobutter@gmail.com>
2026-02-19 23:11:09 -0300

    Update styles and scripts for improved layout and responsiveness: change font families, adjust header size, and enhance event grid layout

--- static/js/header_shrink.js
+++ static/js/header_shrink.js
@@ -5,9 +5,9 @@ function shrinkHeader() {
     var header = document.querySelector("header"); // Seleccionamos el header
     if (window.pageYOffset > 50) { // Si el scroll es mayor a 50px
         header.classList.add("shrunken"); // Añadimos la clase 'shrunken'
-        document.body.style.paddingTop = '60px'; // Ajustamos el padding del body
     } else {
         header.classList.remove("shrunken"); // Quitamos la clase 'shrunken' cuando volvemos arriba
-        document.body.style.paddingTop = '150px'; // Restauramos el padding del body
     }
 }
+
+shrinkHeader();
